451c1828a7d2eb983a4c2e8fbea81b658f553b01
Hessel Middendorp <hesselmiddendorp@iMac-van-Hessel.fritz.box>
2021-09-12 13:22:16 +0200

    efeifhueo

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,39 +1,83 @@
 <template>
   <main>
-    <div class="mx-auto max-w-[375px]">
-      <h1 class="mt-[27px] ml-[44px] text-[40px] leading-[48px]">Contact</h1>
-      <div class="mt-[27px] mr-[50px] ml-[34px]">
-        <h2 class="text-[24px] leading-[29px]">Want to get in touch?</h2>
-        <p class="mt-[19px] text-[16x] leading-[19px]">
+    <div class="mx-auto max-w-[375px] md:w-[768px] md:max-w-none xl:w-[1280px]">
+      <h1
+        class="mt-[27px] ml-[44px] text-[40px] leading-[48px] md:mt-[69px] md:ml-[77px] md:text-[45px] md:leading-[54px] xl:mt-[129px] xl:ml-[101px] xl:text-[50px] xl:leading-[61px]"
+      >
+        Contact
+      </h1>
+      <div
+        class="mt-[27px] mr-[50px] ml-[34px] md:mt-[48px] md:mr-[48px] md:ml-[77px] xl:mt-[42px] xl:mr-auto xl:ml-[101px] xl:w-[676px]"
+      >
+        <h2
+          class="text-[24px] leading-[29px] md:text-[30px] md:leading-[37px] xl:text-[35px] xl:leading-[42px]"
+        >
+          Want to get in touch?
+        </h2>
+        <p
+          class="mt-[19px] text-[16x] leading-[19px] md:text-[22px] md:leading-[27px] xl:text-[25px] xl:leading-[30px]"
+        >
           For press, partner or sponsorship enquires please contact us at
           <a class="underline" href="mailto:info@ddd21.nl">info@ddd21.nl</a>
         </p>
-        <p class="mt-[19px] text-[16x] leading-[19px]">
+        <p
+          class="mt-[19px] text-[16x] leading-[19px] md:mt-[29px] md:text-[22px] md:leading-[27px] xl;mt-[30px] xl:text-[25px] xl:leading-[30px]"
+        >
           connect with us on
-          <a class="underline" href="">LinkedIn</a>
+          <a
+            class="underline"
+            href=""
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+          >
+            LinkedIn
+          </a>
           ,
-          <a class="underline" href="">Instagram</a>
+          <a
+            class="underline"
+            href=""
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+          >
+            Instagram
+          </a>
           or
-          <a class="underline" href="">Facebook</a>
+          <a
+            class="underline"
+            href=""
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+          >
+            Facebook
+          </a>
         </p>
-        <p class="mt-[19px] text-[16x] leading-[19px]">
+        <p
+          class="mt-[19px] text-[16x] leading-[19px] md:text-[22px] md:leading-[27px] xl:mt-[30px] xl:text-[25px] xl:leading-[30px]"
+        >
           Or get in touch with a driver directly:
-          <a class="text-[24px] leading-[29px] underline" href="/the-drivers">
+          <a
+            class="text-[24px] leading-[29px] underline md:text-[30px] md:leading-[37px] xl:text-[35px] xl:leading-[42px]"
+            href="/the-drivers"
+          >
             Meet the Drivers
           </a>
         </p>
-        <p class="mt-[19px] text-[16x] leading-[19px]">
+        <p
+          class="mt-[19px] text-[16x] leading-[19px] md:text-[22px] md:leading-[27px] xl:text-[25px] xl:leading-[30px]"
+        >
           Feeling inspired and want to know more about the program?
 
           <a
-            class="text-[24px] leading-[29px] underline"
+            class="text-[24px] leading-[29px] underline md:text-[30px] md:leading-[37px] xl:text-[35px] xl:leading-[42px]"
             href="https://drivingdutchdesign.nl/"
+            target="_blank"
+            rel="nofollow noreferrer noopener"
           >
             drivingdutchdesign.nl
           </a>
         </p>
         <a
-          class="inline-block mt-[58px] text-[24px] leading-[29px underline"
+          class="inline-block mt-[58px] text-[24px] leading-[29px underline md:text-[30px] md:leading-[37px] xl:text-[35px] xl:leading-[42px]"
           href="/"
         >
           Pressfolder
@@ -42,7 +86,7 @@
     </div>
     <div
       id="grey-stroke"
-      class="mt-[14px] h-[47px] bg-center bg-no-repeat bg-cover"
+      class="mt-[14px] h-[47px] bg-center bg-no-repeat bg-cover md:mt-[17px] md:h-[97px] xl:mt-[7px] xl:h-[159px]"
     ></div>
   </main>
 </template>
